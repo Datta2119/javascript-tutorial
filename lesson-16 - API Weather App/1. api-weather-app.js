6ffee294d566b403f773f5084e48f9fdb1d31d78
Datta2119 <asbishway2119@gmail.com>
2022-11-30 01:01:52 +0600

    Lesson-16 - API Weather App [input field clear issue fixed]

--- lesson-16 - API Weather App/1. api-weather-app.js
+++ lesson-16 - API Weather App/1. api-weather-app.js
@@ -9,9 +9,14 @@ const getWeatherData = (city) => {
 
 
 const searchCity = async () => {
-    const city = document.getElementById('city-input').value;
+    const city = document.getElementById('city-input');
     // console.log(city)
-    const data = await getWeatherData(city)
+    const data = await getWeatherData(city.value)
+
+    if (city.value != '') {
+        city.value = ''
+    }
+
     showWeatherData(data)
 }
 
@@ -21,7 +26,7 @@ const showWeatherData = (weatherData) => {
     document.getElementById('city-name').innerText = weatherData.name
 
     document.getElementById('weather-type').innerText = weatherData.weather[0].main
-    
+
     document.getElementById('temp').innerText = weatherData.main.temp
     document.getElementById('min-temp').innerText = weatherData.main.temp_min
     document.getElementById('max-temp').innerText = weatherData.main.temp_max
